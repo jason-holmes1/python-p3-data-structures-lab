83411a66fdd8f7819c5bbf6a288f63ee65872a6c
Jason <holmesjason481@gmail.com>
2024-03-11 14:07:31 -0400

    finished data structures lab

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,41 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = []
+    for food in spicy_foods:
+        names.append(food["name"])
+    return names
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    spiciest_foods = []
+    for food in spicy_foods:
+        if food["heat_level"] > 5:
+            spiciest_foods.append(food)
+    return spiciest_foods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    pepper = "🌶"
+    for food in spicy_foods:
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {pepper * food['heat_level']}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    spicy_foods_by_cuisine = {}
+    for food in spicy_foods:
+        if food["cuisine"] == cuisine:
+            spicy_foods_by_cuisine.update(food)
+            
+    return(spicy_foods_by_cuisine)
 
 def print_spiciest_foods(spicy_foods):
-    pass
-
+    spice = get_spiciest_foods(spicy_foods)
+    print_spicy_foods(spice)
+#! len = length of the list
 def get_average_heat_level(spicy_foods):
-    pass
-
+   return sum(
+       [food["heat_level"] for food in spicy_foods]
+       ) / len(spicy_foods)#
+#! when adding to a list, it must be done before the return stataement, otherwise it will not be added to the list
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
+    
